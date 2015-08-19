fa7a5b4647a22d6ebf6b23fe923699e8f1da8f53
Michael Lu <miclu@microsoft.com>
2015-08-18 21:32:24 -0700

    Add initial service setup

---
+++ FineHomeMadeFoodNearMe.Services/FineHomeMadeFoodNearMe.Services/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FineHomeMadeFoodNearMe.Services.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FineHomeMadeFoodNearMe.Services/FineHomeMadeFoodNearMe.Services/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FineHomeMadeFoodNearMe.Services.WebApiApplication" Language="C#" %>
