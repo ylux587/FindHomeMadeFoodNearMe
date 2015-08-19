4fb29323fe919fe2143c1fd0a885f410169751a3
ylux587 <ylux587@hotmail.com>
2015-08-19 14:34:07 -0700

    To Resolve conflicts

---
+++ FineHomeMadeFoodNearMe.Services/FineHomeMadeFoodNearMe.Services/DataAccess/TableBinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
+
+namespace FineHomeMadeFoodNearMe.Services.DataAccess
+{
+    public class TableBinder
+    {
+        public SqlParameter BindTable(
+            String parameterName,
+            String typeName,
+            IEnumerable<SqlDataRecord> rows,
+            bool convertEmptyToNull = true)
+        {
+            if (convertEmptyToNull && !rows.Any())
+            {
+                rows = null;
+            }
+
+            SqlParameter param = m_sqlCommand.Parameters.AddWithValue(parameterName, rows);
+
+            param.TypeName = typeName;
+            param.SqlDbType = SqlDbType.Structured;
+
+            return param;
+        }
+    }
+}
