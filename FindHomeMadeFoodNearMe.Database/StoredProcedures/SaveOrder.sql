0c0ca523c5283fa3b95bf9cde7a04c610a83ef9f
ylux587 <ylux587@hotmail.com>
2015-08-20 21:50:09 -0700

    Add missing script

---
+++ FindHomeMadeFoodNearMe.Database/StoredProcedures/SaveOrder.sql
@@ -0,0 +1,42 @@
+﻿CREATE PROCEDURE [dbo].[SaveOrder]
+    @userId BIGINT,
+    @orderDate DATETIME2(7),
+    @subTotal DECIMAL(7,2),
+    @tax DECIMAL(7,2),
+    @otherCharges DECIMAL(7,2),
+    @notes NVARCHAR(1000),
+    @status INT,
+    @orderItems typ_OrderItems_v1 READONLY
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    BEGIN TRAN
+
+    INSERT [dbo].[Orders]
+        (
+            [UserId], 
+            [OrderDate], 
+            [SubTotal], 
+            [Tax], 
+            [OtherCharges], 
+            [Notes],
+            [Status]
+        )
+        VALUES
+        (
+            @userId, 
+            @orderDate, 
+            @subTotal, 
+            @tax, 
+            @otherCharges, 
+            @notes,
+            @status
+        )
+     DECLARE @orderId BIGINT = SCOPE_IDENTITY();
+
+     EXEC SaveOrderItems @orderId, @orderItems
+
+     COMMIT TRAN
+
+END
